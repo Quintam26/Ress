9a09a2348fe0ca2dbb620c61efa536c90538fe95
Mario Quintana <pyrros2000@gmail.com>
2018-11-22 10:28:11 -0800

    added react router

---
+++ src/components/404.js
@@ -0,0 +1,4 @@
+export const W404 = () => 
+  <div>
+    <h1>404</h1>
+  </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 import { App } from './components/App';
+import { W404 } from './components/404';
+import { Router, Route, hashHistory } from 'react-router';
 
 window.React = React
 
 render(
-	<App/>,
+	<Router history={hashHistory}>
+			<Route path="/" component={App}/>
+			<Route path="*" component={W404}/>
+	</Router>,
 	document.getElementById('react-container')
 	)
